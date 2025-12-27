e24e0da7ab79cb713ba009df9f230084f02dec3c
Brandon <samanoch@bk.ru>
2025-12-28 00:56:03 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -134,8 +134,8 @@ function analyzeSalesData(data, options) {
   return sellerStats.map((seller) => ({
     seller_id: seller.id,
     name: seller.name,
-    revenue: seller.revenue / 100,
-    profit: seller.profit / 100,
+    revenue: +seller.revenue.toFixed(2),
+    profit: +seller.profit.toFixed(2),
     sales_count: seller.sales_count,
     top_products: seller.top_products,
     bonus: +seller.bonus.toFixed(2),
